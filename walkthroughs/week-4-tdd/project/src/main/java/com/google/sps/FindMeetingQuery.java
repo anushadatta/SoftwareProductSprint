ec583d860867573df2d36e05521ff28b2545a77b
Anusha Datta <anushadatta@gmail.com>
2020-06-28 15:44:24 +0800

    Scheduling algorithm implementation

--- walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
+++ walkthroughs/week-4-tdd/project/src/main/java/com/google/sps/FindMeetingQuery.java
@@ -14,10 +14,115 @@
 
 package com.google.sps;
 
+import java.util.ArrayList;
+import java.util.Arrays;
 import java.util.Collection;
+import java.util.Collections;
+import java.util.List;
+import java.util.Set;
+import java.util.HashSet;
 
 public final class FindMeetingQuery {
-  public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
-    throw new UnsupportedOperationException("TODO: Implement this method.");
-  }
+    public Collection<TimeRange> query(Collection<Event> events, MeetingRequest request) {
+        
+        // no meeting time available 
+        if (request.getDuration() > TimeRange.WHOLE_DAY.duration()) {
+            return Arrays.asList();
+        }
+        
+        // no other clashing events
+        if (events.isEmpty()) {
+            return Arrays.asList(TimeRange.WHOLE_DAY);
+        }
+
+        // flag to check mutual exclusivity of attendee list
+        boolean distinctAttendees = true;
+
+        List<TimeRange> available = Arrays.asList(TimeRange.fromStartEnd(TimeRange.START_OF_DAY, TimeRange.END_OF_DAY, true));
+
+        for (Event event : events) {
+            if (attendeesOverlap(event.getAttendees(), request.getAttendees())) {
+                available = remove(available, event.getWhen());
+                distinctAttendees = false;
+            }
+        }
+
+        // mutual exclusivity of attendee list, i.e. no clashing events
+        if (distinctAttendees == true) {
+            return Arrays.asList(TimeRange.WHOLE_DAY);
+        }
+
+        return available;
+    }
+
+    public List<TimeRange> remove(Collection<TimeRange> collectionRanges, TimeRange rangeToRemove) {
+        
+        int index = 0;
+        ArrayList<TimeRange> ranges = new ArrayList<>(collectionRanges);
+        
+        for (TimeRange range : ranges) {
+
+            // Case 1: range fully covers rangeToRemove
+            // |---------|       range
+            //   |----|          rangeToRemove
+            if (range.start() < rangeToRemove.start() && range.end() > rangeToRemove.end()) {
+                TimeRange leftRange = TimeRange.fromStartEnd(range.start(), rangeToRemove.start(), false);
+                TimeRange rightRange;
+                
+                if (range.end() >= TimeRange.END_OF_DAY) {
+                   rightRange = TimeRange.fromStartEnd(rangeToRemove.end(), TimeRange.END_OF_DAY, true);
+                } else {
+                    rightRange = TimeRange.fromStartEnd(rangeToRemove.end(), range.end(), true);
+                }
+                
+                ranges.set(index, leftRange);
+                ranges.add(index+1, rightRange);
+                
+                break;
+            }
+
+            // Case 2: range is fully contained in rangeToRemove
+            //   |---|            range
+            // |--------|         rangeToRemove
+            if (range.start() >= rangeToRemove.start() && range.end() <= rangeToRemove.end()) {
+                ranges.remove(index);
+                index--;
+                continue;
+            }
+
+            // Case 3: range starts before but end while rangeToRemove is happening
+            //   |------|         range
+            //      |--------|    rangeToRemove
+            if (range.start() < rangeToRemove.start() && range.end() >= rangeToRemove.start() && range.end() <= rangeToRemove.end()) {
+                TimeRange newRange = TimeRange.fromStartEnd(range.start(), rangeToRemove.start(), false);
+                ranges.set(index, newRange);
+                continue;
+            }
+
+            // Case 4: range ends after but starts while rangeToRemove is happening
+            //   |--------|       range
+            // |------|           rangeToRemove
+            if (range.start() >= rangeToRemove.start() && range.start() <= rangeToRemove.end() && range.end() > rangeToRemove.end()) {
+                TimeRange newRange = TimeRange.fromStartEnd(rangeToRemove.end(), range.end(), false);
+                ranges.set(index, newRange);
+                continue;
+            }
+
+            index++;
+        
+        }
+
+        return ranges;
+    }
+
+    public boolean attendeesOverlap(Set<String> setA, Set<String> setB) {
+        Set<String> intersection = new HashSet<>(setA);
+        intersection.retainAll(setB);
+        
+        if (intersection.isEmpty()) {
+            return false;
+        } else {
+            return true;
+        }
+    }
 }
